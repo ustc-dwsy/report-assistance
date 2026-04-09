44591b59b092f99031e37a2c5655d4b107b33c6b
wsw12321 <2183220676@qq.com>
2026-04-10 00:36:40 +0800

    新增导航栏链接，完善使用指南内容，更新页脚信息。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
-  integrations: [preact({ compat: true })],
+  integrations: [preact({ compat: true }), mdx()],
   output: 'static',
   vite: {
     ssr: {
